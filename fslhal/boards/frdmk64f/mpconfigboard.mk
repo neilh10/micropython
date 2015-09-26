fb1e43ed66fc315ad5e3619b0b8fcbe4f8173418
neilh10 <neilh20+github1509@wLLw.net>
2015-09-26 08:33:42 -0700

    First pass of MK64FN files as generated by KDS300 for a k64f msd_UsbDevMsc (USB to microSD)

--- fslhal/boards/frdmk64f/mpconfigboard.mk
+++ fslhal/boards/frdmk64f/mpconfigboard.mk
@@ -3,4 +3,4 @@ CMSIS_MCU = mk64fn1M0xxx12
 AF_FILE = boards/mk64fn1M0xxx12_af.csv
 LD_FILE = boards/mk64fn1M0xxx12.ld
 #LDFLAGS_MPCONFIG =  -Xlinker --gc-sections -Wl, -Xlinker -z  -Xlinker muldefs  -Xlinker --defsym=__ram_vector_table__=1  --specs=nano.specs          -Wall  -fno-common  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin  -Os  -mapcs  -Xlinker -static  -Xlinker --defsym=__stack_size__=0x1000  -Xlinker --defsym=__heap_size__=0x0400
-CFLAGS_MPCONFIG = -DF_CPU=96000000 -DUSB_SERIAL -D__MK20DX256__
+CFLAGS_MPCONFIG = -DF_CPU=96000000 -DUSB_SERIAL -DCPU_MK64FN1M0VLL12
